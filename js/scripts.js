6d674ce98d65540158d19af78d4e2b9497d8a06d
Jordan Meier <jbirdmeier@gmail.com>
2016-01-22 08:18:05 -0800

    Add functionality to first test, where numbers divisible by 3 will return 'ping'

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var pingPong = function (number) {
+  if (number % 3 === 0) {
+    return "ping";
+  }
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('pingPong', function () {
+  it("is a number divisible by 3", function () {
+    expect(pingPong(6)).to.equal("ping");
+  });
+});
